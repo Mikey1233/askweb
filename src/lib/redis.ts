c1fbc71d8f71809d9d31fa218b391cd1e2baf406
michael1233 <emmamichael201@gmail.com>
2024-12-21 22:31:03 +0100

    refactor: update Redis initialization and configuration

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,3 +1,13 @@
-import {Redis} from "@upstash/redis"
+// import {Redis} from "@upstash/redis"
 
-export const redis = Redis.fromEnv()
+// export const redis = Redis.fromEnv()
+
+import { Redis } from '@upstash/redis'
+
+export const redis = new Redis({
+  url: 'https://gusc1-coherent-adder-30283.upstash.io',
+  token: 'AXZLASQgYzIzMjQzZjQtNjI2NC00ZThiLTg5NWUtODBlOGFjYWNmZTFkMmI1ZjhlOTdiYmM2NDhmOWIxMDU0OWE2ZTIyMTQyMTY=',
+})
+
+// await redis.set('foo', 'bar');
+// const data = await redis.get('foo');
